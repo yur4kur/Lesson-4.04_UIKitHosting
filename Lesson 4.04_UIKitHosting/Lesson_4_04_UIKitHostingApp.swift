0ba2437ac514d385d84c4a4d969e10245b9038fc
Iurii Kurinnoi <yur4kur@icloud.com>
2023-12-20 12:43:31 +0400

    Initial Commit

---
+++ Lesson 4.04_UIKitHosting/Lesson_4_04_UIKitHostingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_04_UIKitHostingApp.swift
+//  Lesson 4.04_UIKitHosting
+//
+//  Created by Юрий Куринной on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_04_UIKitHostingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
